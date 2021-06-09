af25300a7661227fa0c179d91dcaba89d1f71cfa
William David <william-cwb@hotmail.com>
2021-06-08 22:07:20 -0300

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by William David on 08/06/21.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
